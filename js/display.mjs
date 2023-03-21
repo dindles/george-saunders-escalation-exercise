9b03450eca71da99d87dcc029ae336b3809216c1
dindles <114931816+dindles@users.noreply.github.com>
2023-03-21 09:11:01 +0200

    Add goButtonGo transitions - needs work

--- js/display.mjs
+++ js/display.mjs
@@ -2,16 +2,58 @@
 function goButtonGo() {
   toggle_visibility_main();
   toggle_visibility_intro();
+
+  setTimeout(function () {
+    transition_main();
+    transition_intro();
+  }, 10); // wait x ms before starting the transitions
 }
-// go button makes textare and wordcounts visible
+
 function toggle_visibility_main() {
-  var e = document.getElementById("main");
+  const e = document.getElementById("main");
   e.style.display = "flex";
+  e.style.opacity = 0;
+  e.style.transform = "scale(0)";
 }
+
 function toggle_visibility_intro() {
-  var e = document.getElementById("intro");
-  e.style.display = "none";
+  const e = document.getElementById("intro");
+  e.style.display = "block";
+  e.style.opacity = 1;
+  e.style.transform = "scale(1)";
 }
+
+function transition_main() {
+  const e = document.getElementById("main");
+  let opacity = 0;
+  let scale = 0;
+  const interval = setInterval(function () {
+    opacity += 0.1;
+    scale += 0.1;
+    e.style.opacity = opacity;
+    e.style.transform = "scale(" + scale + ")";
+    if (opacity >= 1 || scale >= 1) {
+      clearInterval(interval);
+    }
+  }, 20); // animate every x ms
+}
+
+function transition_intro() {
+  const e = document.getElementById("intro");
+  let opacity = 1;
+  let scale = 1;
+  const interval = setInterval(function () {
+    opacity -= 0.1;
+    scale -= 0.1;
+    e.style.opacity = opacity;
+    e.style.transform = "scale(" + scale + ")";
+    if (opacity <= 0 || scale <= 0) {
+      clearInterval(interval);
+      e.style.display = "none"; // hide element when transition is complete
+    }
+  }, 10); // animate every x ms
+}
+
 // go button makes intro para, title and go button hidden
 // go button makes thin bar appear top with clock, save and collapsible
 // collapsible shows intro para
