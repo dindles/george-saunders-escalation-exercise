5475ee68ee8761ea290a5fb1bfd3217dffc2c4cb
dindles <114931816+dindles@users.noreply.github.com>
2023-03-21 08:04:51 +0200

    Add hidden main

--- js/display.js
+++ js/display.js
@@ -1,3 +1,14 @@
+// INTRO/STORY DISPLAY LOGIC
+
+// go button makes textare and wordcounts visible
+
+// go button makes intro para, title and go button hidden
+// go button makes thin bar appear top with clock, save and collapsible
+// https://www.w3schools.com/howto/howto_js_collapsible.asp
+// collapsible shows intro para
+
+// OTHER
+// textarea height
 const storyText = document.getElementById("story-text");
 
 storyText.addEventListener("input", () => {
